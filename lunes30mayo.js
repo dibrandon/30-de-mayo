bb4181aec5ad829e6b62de478e34e2489ffd4223
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-05-30 17:10:27 +0200

    iteracion 2

---
+++ lunes30mayo.js
@@ -0,0 +1,30 @@
+// Iteration 1: Names and Input
+let hacker1 = "Alonso"
+let hacker2 = "Alonso"
+
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is ${hacker2}`)
+
+console.log("----------------------")
+console.log("iteracion 2 a continuacion");
+
+// Iteration 2: Conditionals
+
+
+if (hacker1.length > hacker2.length){
+   console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}else if(hacker2.length > hacker1.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}else{
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
+
+// Iteration 3: Loops
+
+
+
+
+
+
+
+
